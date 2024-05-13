80bb6cb2f52c5ad51c7490e7e07c6ad89a9d7bb1
munyphalla <lalaphalla@gmail.com>
2024-05-13 18:18:16 +0700

    feature: create Color Component

---
+++ pages/product-detail/components/index.js
@@ -0,0 +1,20 @@
+// Color Component
+class ColorButton extends HTMLElement{
+    constructor(){
+        super()
+    }
+    connectedCallback(){
+        const title = this.dataset.title
+        const textColor = this.dataset.color
+
+        this.innerHTML = `
+            <span
+                class="w-8 h-8 rounded-full border block border-black bg-${textColor}"
+                ></span>
+                <p>${title}</p>
+                <p>$189.99</p> 
+        `
+        this.classList = "flex flex-col justify-center items-center border-2 w-[49%] border-[#ddd] rounded-md mt-2 p-3 cursor-pointer"
+    }
+}
+customElements.define('color-button', ColorButton);
