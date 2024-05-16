40b4525827cced379231a3500f25074fa28ede4f
finnick203 <songseyla99@gmail.com>
2024-05-16 15:57:20 +0700

    transform tv sectinon into components

--- pages/home/components/carousel-data.js
+++ pages/home/components/carousel-data.js
@@ -1,5 +1,4 @@
 export const mobileData = {
-    tab : 1,
     headerText: "Mobile & Computing",
     items: [
         {
@@ -23,14 +22,14 @@ export const mobileData = {
         {
             "title": "Galaxy Tab S9 Series",
             "description": "Get up to $750 trade-in credit and $75 instant Samsung Credit with participating carriers. Try it for 30 Days or get a full refund.",
-            "largeImage": "../../assets/images/home/mobile/laptop_big.png",
-            "smallImage": "../../assets/images/home/mobile/laptop_small.png"
+            "largeImage": "../../assets/images/home/mobile/tablet_big.jpeg",
+            "smallImage": "../../assets/images/home/mobile/tablet_small.jpeg"
         },
         {
             "title": "Galaxy Watch6 Series",
             "description": "Get up to $750 trade-in credit and $75 instant Samsung Credit with participating carriers. Try it for 30 Days or get a full refund.",
-            "largeImage": "../../assets/images/home/mobile/laptop_big.png",
-            "smallImage": "../../assets/images/home/mobile/laptop_small.png"
+            "largeImage": "../../assets/images/home/mobile/watch_big.jpeg",
+            "smallImage": "../../assets/images/home/mobile/watch_small.jpeg"
         }
     ],
     buttons: [
@@ -41,44 +40,34 @@ export const mobileData = {
         "Galaxy Watch6 Series"
     ]
 };
+
+
+
 export const tvData = {
-    tab : 1,
-    headerText: "Mobile & Computing",
+    headerText: "TV & Audio",
     items: [
         {
-            "title": "Galaxy S24 Ultra",
-            "description": "Get up to $750 trade-in credit and $75 instant Samsung Credit with participating carriers. Try it for 30 Days or get a full refund.",
-            "largeImage": "../../assets/images/home/mobile/s24_ultra_big.avif",
-            "smallImage": "../../assets/images/home/mobile/s24_ultra_small.avif"
+            "title": "TV with a canvas-like finish",
+            "description": "Experience Anti-Reflection with Matte Display. Save up to $200 on a 75 Class The Frame.",
+            "largeImage": "../../assets/images/home/tv/frame-big.png",
+            "smallImage": "../../assets/images/home/tv/frame-small.png"
         },
         {
-            "title": "Galaxy S24+",
-            "description": "Get up to $750 trade-in credit and $75 instant Samsung Credit with participating carriers. Try it for 30 Days or get a full refund.",
-            "largeImage": "../../assets/images/home/mobile/s24_big.jpeg",
-            "smallImage": "../../assets/images/home/mobile/s24_small.jpeg"
+            "title": "TV made for mobile",
+            "description": "Cast your phone's apps and enjoy your faviorite TV content in both landscape or portrait mode. Save up to $200 on a 43 Class The Sero QLED 4K (2022).",
+            "largeImage": "../../assets/images/home/tv/phone-big.jpeg",
+            "smallImage": "../../assets/images/home/tv/phone-small.jpeg"
         },
         {
-            "title": "Galaxy Book4 Ultra",
-            "description": "Get up to $750 trade-in credit and $75 instant Samsung Credit with participating carriers. Try it for 30 Days or get a full refund.",
-            "largeImage": "../../assets/images/home/mobile/laptop_big.png",
-            "smallImage": "../../assets/images/home/mobile/laptop_small.png"
-        },
-        {
-            "title": "Galaxy Tab S9 Series",
-            "description": "Get up to $750 trade-in credit and $75 instant Samsung Credit with participating carriers. Try it for 30 Days or get a full refund.",
-            "largeImage": "../../assets/images/home/mobile/laptop_big.png",
-            "smallImage": "../../assets/images/home/mobile/laptop_small.png"
-        },
-        {
-            "title": "Galaxy Watch6 Series",
-            "description": "Get up to $750 trade-in credit and $75 instant Samsung Credit with participating carriers. Try it for 30 Days or get a full refund.",
-            "largeImage": "../../assets/images/home/mobile/laptop_big.png",
-            "smallImage": "../../assets/images/home/mobile/laptop_small.png"
+            "title": "Compact design, captivating sound",
+            "description": "Powerful Dolby Atmos sound in an impressively slender package. Shop our Ultra Slim Soundbar.",
+            "largeImage": "../../assets/images/home/tv/sound-big.jpeg",
+            "smallImage": "../../assets/images/home/tv/sound-big.jpeg"
         }
     ],
     buttons: [
-        "Galaxy S24 Ultra",
-        "Galaxy S24+",
-        "Galaxy Watch6 Series"
+        "The Frame LS03D 75",
+        "43 LS05 Sero",
+        "Ultra Slim Soundbar"
     ]
 };
