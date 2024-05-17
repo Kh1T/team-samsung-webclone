442522199d27a73640908fab9556d3fc83a365b6
finnick203 <songseyla99@gmail.com>
2024-05-17 14:36:36 +0700

    finish carousel tab

--- pages/home/components/carousel-homepage.js
+++ pages/home/components/carousel-homepage.js
@@ -1,15 +1,12 @@
-
-import { mobileData } from './carousel-data.js';
-import { tvData } from './carousel-data.js';
-import { homeData } from './carousel-data.js';
-import { susData } from './carousel-data.js';
+import { mobileData, tvData, homeData, susData } from './carousel-data.js';
 
 
 
 export class CarouselTab extends HTMLElement {
     connectedCallback() {
 
-                // get attribute from html
+        // get attribute from html
+        
         const dataType = this.getAttribute('data-type');
         let data;
         
@@ -28,42 +25,56 @@ export class CarouselTab extends HTMLElement {
         const { items, headerText, buttons } = data; // Unpacking Data
         
         this.innerHTML = `
-            <section class="mobile max-w-screen-2xl w-screen">
-                <div class="mobile-container relative">
-                    <div class="flex overflow-hidden gap-4 ">
-                        <div id="mobile-container" class="flex transition-transform duration-500">
-                            ${ // Map through items from data file 
-                                items.map(item => `
-                                <div class="item w-full h-full relative flex-none">
-                                    <img class="hidden sm:block w-full" src="${item.largeImage}" alt="">
-                                    <img class="sm:hidden w-full" src="${item.smallImage}" alt="">
-                                    <div class="info flex flex-col sm:gap-2 text-center items-center justify-center pb-6 absolute bottom-8 sm:bottom-0 left-24 right-24">
-                                        <h1 class="text-[6vw] sm:text-[3vw] lg:text-3xl pb-2">${item.title}</h1>
-                                        <p class="text-[2vw] sm:text-[1vw] lg:text-xl">${item.description}</p>
-                                        <button class="btn bg-black p-2 rounded-full text-white font-semibold text-[1.5vw] sm:text-lg">Buy Now</button>
-                                    </div>
+        <section class="carousel max-w-screen-2xl w-screen">
+            <div class="carousel-container relative">
+                <div class="flex overflow-hidden gap-4">
+                    <div id="carousel-inner-container" class="flex transition-transform duration-500">
+                        ${items.map(item => `
+                            <div class="item w-full h-full relative flex-none">
+                                <img class="hidden sm:block w-full" src="${item.largeImage}" alt="">
+                                <img class="sm:hidden w-full" src="${item.smallImage}" alt="">
+                                <div class="info flex flex-col sm:gap-2 text-center items-center justify-center pb-6 absolute bottom-8 sm:bottom-0 left-24 right-24">
+                                    <h1 class="text-[6vw] sm:text-[3vw] lg:text-3xl pb-2">${item.title}</h1>
+                                    <p class="text-[2vw] sm:text-[1vw] lg:text-xl">${item.description}</p>
+                                    <button class="btn bg-black p-2 rounded-full text-white font-semibold text-[1.5vw] sm:text-lg">Buy Now</button>
                                 </div>
-                            `).join('')}
-                        </div>
+                            </div>
+                        `).join('')}
                     </div>
-                    <div class="header mt-8 absolute top-0 left-1/2 transform -translate-x-1/2 overflow-auto">
-                        <h2 class="text-3xl sm:text-[3vw] lg:text-5xl font-semibold text-center">${headerText}</h2>
-                        <div class="flex flex-col items-center gap-4 scrollbar-none" >
-                            <div class="flex text-nowrap justify-center space-x-10 mt-5 font-bold sm:text-[1.5vw] lg:text-xl">
-                                <div id="btn-mobile"  class="overflow-auto sm:justify-center flex gap-12 px-4 w-screen scrollbar-none" >
-                                    ${  // Map button base on data file
-                                        buttons.map(button => `
-                                        <button onclick="tabCarousel(this)" class="btn-mobile">${button}</button>
-                                    `).join('')}
-                                </div>
+                </div>
+                <div class="header mt-8 absolute top-0 left-1/2 transform -translate-x-1/2 overflow-auto">
+                    <h2 class="text-3xl sm:text-[3vw] lg:text-5xl font-semibold text-center">${headerText}</h2>
+                    <div class="flex flex-col items-center gap-4 scrollbar-none">
+                        <div class="flex text-nowrap justify-center space-x-10 mt-5 font-bold sm:text-[1.5vw] lg:text-xl">
+                            <div id="btn-carousel" class="overflow-auto sm:justify-center flex gap-12 px-4 w-screen scrollbar-none">
+                                ${buttons.map((button, index) => `
+                                    <button data-index="${index}" class="carousel-btn">${button}</button>
+                                `).join('')}
                             </div>
                         </div>
                     </div>
                 </div>
-            </section>
-        `;
+            </div>
+        </section>
+    `;
+
+    const container = this.querySelector('#carousel-inner-container');
+    const btns = this.querySelectorAll('.carousel-btn');
+    carouselTab(container, btns);
     }
 }
 
 customElements.define('carousel-tab', CarouselTab);
 
+
+// For carousel
+
+function carouselTab(container, btns) {
+    btns.forEach(btn => {
+        btn.addEventListener('click', () => {
+            const index = btn.getAttribute('data-index');
+            console.log(index)
+            container.style.transform = `translateX(-${index}00%)`;
+        });
+    });
+}
