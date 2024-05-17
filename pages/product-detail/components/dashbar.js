35fc2721c48e297db8634847937f7dfded29367e
munyphalla <lalaphalla@gmail.com>
2024-05-17 16:08:16 +0700

    feat: dash barcomponent update

--- pages/product-detail/components/dashbar.js
+++ pages/product-detail/components/dashbar.js
@@ -16,10 +16,23 @@ export class DashBar extends HTMLElement{
         }
         const barInner = document.createElement('div')
         barInner.classList = "flex absolute gap-0.5 left-0 top-0"
-        for (let i = 0; i < rate; i++){
-            const contentHtml = `
-                <div class="bar-inner h-2.5 w-14 md:w-8 bg-orange-400"></div>
+        let percent = parseFloat(rate) - parseInt(rate)
+        let intRate = parseInt(rate)
+        percent = parseInt(percent * 100)
+        console.log('intRate',intRate)
+        for (let i = 1; i <= intRate; i++){            
+            let contentHtml = `
+                <div class="bar-inner h-2.5 w-14 md:w-8 relative ">
+                    <div class="h-2.5 w-full bg-orange-400"></div>
+                </div>
             `
+            console.log(i,percent);
+            if (i == intRate  && percent > 0 ){
+                contentHtml += `<div class="bar-inner h-2.5 w-14 md:w-8 relative ">
+                    <div class="h-2.5 w-[${percent}%] bg-orange-400"></div>
+                </div>
+                `
+            }
             barInner.innerHTML += contentHtml            
         }
         this.classList += " flex gap-0.5 relative item-center justify-center"
