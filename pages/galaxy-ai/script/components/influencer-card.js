867f68eb6cf7d81a08d5cce569d38c874c94f727
sela-bet <betsela555@gmail.com>
2024-05-20 16:50:17 +0700

    add: implement galaxy-ai skeleton and basic style

--- pages/galaxy-ai/script/components/influencer-card.js
+++ pages/galaxy-ai/script/components/influencer-card.js
@@ -3,7 +3,14 @@ export class InfluencerCard extends HTMLElement{
         super()
     }
     connectedCallback(){
-        this.innerHTML = '<h1>Influencer Card</h2>'
+        const image = this.dataset.image
+        this.innerHTML = `
+        <div class="flex flex-col gap-4 h-fit items-center">
+        <img src="${image}" alt="">
+        <a href="#" class="flex gap-2 bg-black w-fit text-white px-6 py-3 rounded-full text-sm">Watch now 
+            <img src="../../assets/images/galaxy-ai/icon/redirect.svg" >
+        </a>
+        </div>`
     }
 }
 
--- pages/galaxy-ai/script/components/product-series-card.js
+++ pages/galaxy-ai/script/components/product-series-card.js
@@ -1,9 +1,25 @@
-export class ProductSeriesCard extends HTMLElement{
-    constructor(){
+export class ProductSeriesCard extends HTMLElement {
+    constructor() {
         super()
     }
-    connectedCallback(){
-        this.innerHTML = '<h1>Product Series Card</h2>'
+    connectedCallback() {
+        const heading = this.dataset.heading
+        const subheading = this.dataset.subheading
+        const image = this.dataset.image
+        const description = this.dataset.description
+        const reverse = this.dataset.reverse
+
+        this.classList.add("w-full")
+        this.innerHTML = `
+        <div class="flex py-12 justify-between ${reverse} items-center">
+            <div class="max-w-lg w-1/2 space-y-4">
+                <p class="font-medium text-sm">${subheading.toUpperCase()}</p>
+                <h2 class="text-4xl font-bold">${heading}</h2>
+                <p>${description}</p>
+            </div>
+            <img class="w-[600px] h-fit object-fill bg-contain" src="${image}" alt="feature-image" >
+        </div>
+        `
     }
 }
 
