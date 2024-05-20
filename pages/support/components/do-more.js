c9f2b1e1416d50471569d586f9f2b34ddbaa8089
sovanchitra <chivsovanchetra@gmail.com>
2024-05-20 16:59:11 +0700

    support: make component on do more section

---
+++ pages/support/components/do-more.js
@@ -0,0 +1,23 @@
+class DoMore extends HTMLElement {
+    constructor() {
+        super();
+    }
+
+    connectedCallback() {
+        const image = this.dataset.image;
+        const text = this.dataset.text;
+        const colspan = this.dataset.colspan;
+        const width = this.dataset.width;
+        const height = this.dataset.height;
+
+        this.innerHTML = `
+            <div class="${colspan} relative w-screen flex justify-center">
+                <img class="w-${width} h-${height}" src="${image}" alt="">
+                <p class="absolute top-0">${text}</p>
+            </div>
+        `
+    }
+}
+const
+
+customElements.define('grid-item', DoMore);
