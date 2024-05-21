3e72d466aeefc2d50700057ef3cd9ca2f9e5e215
sela-bet <betsela555@gmail.com>
2024-05-21 15:44:08 +0700

    add: implement component for product series screen

--- pages/galaxy-ai/script/components/product-series-card.js
+++ pages/galaxy-ai/script/components/product-series-card.js
@@ -3,21 +3,19 @@ export class ProductSeriesCard extends HTMLElement {
         super()
     }
     connectedCallback() {
-        const heading = this.dataset.heading
-        const subheading = this.dataset.subheading
         const image = this.dataset.image
-        const description = this.dataset.description
-        const reverse = this.dataset.reverse
+        const name = this.dataset.name
 
-        this.classList.add("w-full")
-        this.innerHTML = `
-        <div class="flex py-12 justify-between ${reverse} items-center">
-            <div class="max-w-lg w-1/2 space-y-4">
-                <p class="font-medium text-sm">${subheading.toUpperCase()}</p>
-                <h2 class="text-4xl font-bold">${heading}</h2>
-                <p>${description}</p>
+        this.innerHTML =  `
+        <div class="flex flex-col p-4 justify-between bg-gray-50 gap-8 rounded-xl items-start h-fit">
+            <div class="flex flex-col gap-4">
+                <img class="object-cover bg-cover h-full w-fit" src="${image}" alt="galaxy watch6 classic">
+                <a class="font-semibold text-2xl href="#">${name}</a>
             </div>
-            <img class="w-[600px] h-fit object-fill bg-contain" src="${image}" alt="feature-image" >
+            <div class=" flex p-4 justify-start w-full text-sm items-center gap-2 font-semibold">
+             <a href="#">Buy Now </a>
+             <img src="../../assets/images/galaxy-ai/icon/chevron-right.svg" alt="icon">
+            </div>
         </div>
         `
     }
