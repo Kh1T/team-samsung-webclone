b147beea339286490c9755f4368cf6b2a4f3914d
finnick203 <songseyla99@gmail.com>
2024-05-23 13:31:11 +0700

    enable components

--- script/index.js
+++ script/index.js
@@ -1,13 +1,11 @@
-// import { NavBar } from './components/navbar.js';
-// import { Footer } from './components/footer.js';
-// import { Chat } from './components/chat.js';
-// import { Feedback } from './components/feedback.js'
-import { CarouselTab } from '../pages/home/components/carousel-homepage.js';
+import { NavBar } from './components/navbar.js';
+import { Footer } from './components/footer.js';
+import { Chat } from './components/chat.js';
+import { Feedback } from './components/feedback.js'
 
-// customElements.define("navbar-component", NavBar);
-// customElements.define("footer-component", Footer);
 
-// customElements.define("chat-component", Chat);
-// customElements.define("feedback-component", Feedback);
+customElements.define("navbar-component", NavBar);
+customElements.define("footer-component", Footer);
 
-customElements.define('carousel-tab', CarouselTab);
+customElements.define("chat-component", Chat);
+customElements.define("feedback-component", Feedback);
