660335a0b045421864d99128ef432d8828985067
munyphalla <lalaphalla@gmail.com>
2024-05-24 15:39:41 +0700

    feat: mobile carousel component

--- pages/smartphone/components/carousel-mobile.js
+++ pages/smartphone/components/carousel-mobile.js
@@ -4,17 +4,24 @@ export class CarouselMobile extends HTMLElement{
         super()
     }
     connectedCallback(){
-        const carosuelData = this.featureData
-
+        const section = this.getAttribute('section')
+        let carosuelData = []
+        if (section === 'connected'){
+            carosuelData = this.connectedData   
+        }
+        else{
+            carosuelData = this.featureData
+        }
+        const favMobileContainer = document.createElement('div')
         const carouselContainer = document.createElement('div')
-        console.log(carosuelData);
+        carouselContainer.classList += ' carousel-container overflow-x-hidden'
 
-        console.log(carosuelData);
+        
         carosuelData.forEach(data =>{
             const cardHTML = `
-            <div class="slide flex min-w-full flex-col items-center">
+            <div class="slide flex min-w-full flex-col items-center mb-6">
                 <img
-                class="w-10/12"
+                class="w-10/12 rounded-xl" 
                 src="${data.imgSrc}"
                 alt="feature"
                 />
@@ -27,11 +34,43 @@ export class CarouselMobile extends HTMLElement{
             </div>
             `;
 
-            this.innerHTML += cardHTML;
+            favMobileContainer.innerHTML += cardHTML;
         })
         
+        carouselContainer.append(favMobileContainer)
+        this.append(carouselContainer)
+        favMobileContainer.classList.add("fav-container-mobile", "flex", "md:hidden", "flex", "transition-transform", "duration-500")
 
-        this.classList.add("fav-container-mobile", "flex", "md:hidden", "flex", "transition-transform", "duration-500")
+
+        // indicator
+
+        const indicatorHandler = (i) => {
+            const btnIndicators = indicatorContainer.querySelectorAll('button')
+            favMobileContainer.style.transform = `translateX(-${i * 100}%)`
+            btnIndicators.forEach(btnIndicator=>{
+                btnIndicator.classList.remove('bg-black')
+                btnIndicator.classList.add('bg-gray-500')            
+            })
+            btnIndicators[i].classList.remove('bg-gray-500')
+            btnIndicators[i].classList.add('bg-black')
+        }
+
+        const slideLength = carouselContainer.querySelectorAll('.slide').length
+        const indicatorContainer = document.createElement('div')
+        for (let i = 0; i < slideLength; i++){
+            const btnIndicator = document.createElement('button')
+            btnIndicator.classList += ` btn-slide mx-3 size-3 rounded-full`
+            if (i === 0) btnIndicator.classList.add('bg-black')
+            else btnIndicator.classList.add('bg-gray-500')
+            indicatorContainer.append(btnIndicator)
+            btnIndicator.addEventListener('click', () => indicatorHandler(i) )
+            // const btnIndicator = document.createElement('button')
+            // btnIndicator.classList += ` btn-slide mx-3 size-3 rounded-full bg-gray-500`
+            // indicatorContainer.append(btnIndicator)
+        }
+        indicatorContainer.classList += " w-fit mx-auto flex rounded-full border border-gray-300 bg-white opacity-80 py-2"
+
+        this.append(indicatorContainer)
         this.classList.add("block")
         
     }
@@ -61,6 +100,23 @@ export class CarouselMobile extends HTMLElement{
             imgSrc : "../../assets/images/smartphone/feat-5.jpg",
             desc : "Switch phone numbers, travel stress-free and more with Galaxy eSIM and Dual SIM capabilities."
         },
+    ]
+    connectedData = [
+        {
+            title : "Call/Text from any device",
+            imgSrc : "../../assets/images/smartphone/connect-1.jpg",
+            desc : "Never miss an important message again. Call and text across your phone, tablet, watch and PC."
+        },
+        {
+            title : "Quick Share large files",
+            imgSrc : "../../assets/images/smartphone/connect-2.jpg",
+            desc : "No more emailing files to yourself. Directly share large files between nearby devices with just a few taps.",
+        },
+        {
+            title : "Upgraded video calls",
+            imgSrc : "../../assets/images/smartphone/connect-3.jpg",
+            desc : "Want more space to connect face-to-face? Transfer Google duo video calls from phone to laptop or tablet. "
+        }
     ]
 }
 
