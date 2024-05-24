5cecdedb981086f23dd995d4abcc2d47f9152f20
sovanchitra <chivsovanchetra@gmail.com>
2024-05-24 18:44:02 +0700

    fix: resolve conflict

--- script/index.js
+++ script/index.js
@@ -1,18 +1,5 @@
 import { NavBar } from './components/navbar.js';
 import { Footer } from './components/footer.js';
-<<<<<<< HEAD
-import { Chat } from './components/chat.js';
-import { Feedback } from './components/feedback.js'
-// import { CarouselTab } from '../pages/home/components/carousel-homepage.js';
-
-customElements.define("navbar-component", NavBar);
-customElements.define("footer-component", Footer);
-
-customElements.define("chat-component", Chat);
-customElements.define("feedback-component", Feedback);
-
-// customElements.define('carousel-tab', CarouselTab);
-=======
 import { Feedback } from './components/feedback.js'
 import { CarouselMobile } from './components/carousel-mobile.js';
 
@@ -20,4 +7,3 @@ customElements.define("navbar-component", NavBar);
 customElements.define("footer-component", Footer);
 customElements.define("feedback-component", Feedback);
 customElements.define("carousel-mobile", CarouselMobile);
->>>>>>> develop
