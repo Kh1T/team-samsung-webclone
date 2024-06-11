f6ec6029f8857dd0c1ea6fff36052ca81b3a9452
sela-bet <betsela555@gmail.com>
2024-06-11 18:30:39 +0700

    refractor: add comment to feedback

--- script/components/feedback.js
+++ script/components/feedback.js
@@ -1,3 +1,23 @@
+/**
+ * Feedback component.
+ * 
+ * This script defines a custom HTML element for a feedback component.
+ * The Feedback class extends HTMLElement and displays a feedback button positioned at the bottom-right corner of the screen.
+ * 
+ * Feedback class:
+ * - Extends HTMLElement.
+ * - Inserts HTML content for the feedback button in the connectedCallback method.
+ * 
+ * HTML content:
+ * - The feedback button is enclosed in a <section> element with fixed positioning at the bottom-right corner of the screen.
+ * - The button itself consists of a black background with rounded corners and a white border.
+ * - Inside the button, a rotated text "Feedback" is displayed in white color.
+ * 
+ * Usage:
+ * The custom element is defined as "feedback-component" using customElements.define.
+ * It can be included in HTML documents to add a feedback button to the user interface.
+ */
+
 export class Feedback extends HTMLElement {
   constructor() {
     super();
