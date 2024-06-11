21dcb9ffd6cb96c50858c89edabf4f425229b34b
sela-bet <betsela555@gmail.com>
2024-06-11 14:51:55 +0700

    refractor: extract inline script to seperate js file

--- script/components/navbar.js
+++ script/components/navbar.js
@@ -70,39 +70,8 @@ export class NavBar extends HTMLElement {
                 </div>
             </section>
         </section>
-
         `
-        const html = ` 
-        <script>
-            const navLinks = document.querySelector('.nav-link')
-            const subNavLinks = document.querySelector('.sub-nav-link') 
-
-            const smallSubNavLinkLayer = document.querySelector('.small-nav-link-layer')
-            const smallSubNavLink = document.querySelector('.small-sub-nav-link')
-            const smallNavLink = document.querySelector('.small-nav-link')
-
-            
-
-            function onSmallNavLinkClick(e) {
-                smallSubNavLinkLayer.classList.toggle('opacity-100')
-                smallNavLink.classList.toggle('w-screen')
-                smallNavLink.classList.toggle('right-0')
-                smallSubNavLink.classList.toggle('right-0')
-                smallSubNavLink.classList.toggle('right-[-500px]')
-                document.querySelector('body').classList.toggle('fixed')
-            }
-
-            function onNavItemOver(e) {
-                
-                navLinks.classList.add('h-dvh')
-            }
-            function onNavItemLeave(e) {
-                navLinks.classList.remove('h-dvh')
-            }
-            subNavLinks.addEventListener('mouseover', onNavItemOver)
-            subNavLinks.addEventListener('mouseleave', onNavItemLeave)
-            
-        </script>`
+        const html = `<script src="../../script/components/navbar-behaviour.js"></script>`
         const scriptEl = document.createRange().createContextualFragment(html);
         this.append(scriptEl);
 
