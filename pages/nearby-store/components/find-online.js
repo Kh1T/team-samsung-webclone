24540c5055ce85d47f747bc10c7450f7c815b5f8
sela-bet <betsela555@gmail.com>
2024-06-11 20:01:15 +0700

    refractor: refractor document with prettier

--- pages/nearby-store/components/find-online.js
+++ pages/nearby-store/components/find-online.js
@@ -3,49 +3,60 @@
  * @type {Array.<{price: string, image: string}>}
  */
 const stocks = [
-    {
-        price: "$189.99",
-        image: "/assets/images/nearby-store/nearby-store2.png",
-    },
-    {
-        price: "$189.99",
-        image: "/assets/images/nearby-store/nearby-store-3-v2.png",
-    },
-    {
-        price: "$229.99",
-        image: "/assets/images/nearby-store/nearby-store-Target.png",
-    },
-    {
-        price: "$189.99",
-        image: "/assets/images/nearby-store/nearby-store-abt.jpeg",
-    },
-    {
-        price: "$160",
-        image: "/assets/images/nearby-store/nearby-store-4.jpeg",
-    }
+  {
+    price: "$189.99",
+    image: "/assets/images/nearby-store/nearby-store2.png",
+  },
+  {
+    price: "$189.99",
+    image: "/assets/images/nearby-store/nearby-store-3-v2.png",
+  },
+  {
+    price: "$229.99",
+    image: "/assets/images/nearby-store/nearby-store-Target.png",
+  },
+  {
+    price: "$189.99",
+    image: "/assets/images/nearby-store/nearby-store-abt.jpeg",
+  },
+  {
+    price: "$160",
+    image: "/assets/images/nearby-store/nearby-store-4.jpeg",
+  },
 ];
 
 /**
  * Executes when the DOM is fully loaded.
- * Iterates over the stocks array and creates HTML elements for each stock item, 
+ * Iterates over the stocks array and creates HTML elements for each stock item,
  * then appends them to the stockList element.
  */
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
+  /**
+   * The container element where stock items will be appended.
+   * @type {HTMLElement}
+   */
+  const stockList = document.getElementById("stockList");
+
+  stocks.forEach((stock) => {
     /**
-     * The container element where stock items will be appended.
+     * Creates a new div element to represent a stock item.
      * @type {HTMLElement}
      */
-    const stockList = document.getElementById("stockList");
+    const stockItem = document.createElement("div");
+    stockItem.classList.add(
+      "stock-item",
+      "text-center",
+      "flex",
+      "flex-col",
+      "items-center",
+      "box-border",
+      "h-44",
+      "shadow-md",
+      "bg-white",
+      "w-44",
+    );
 
-    stocks.forEach((stock) => {
-        /**
-         * Creates a new div element to represent a stock item.
-         * @type {HTMLElement}
-         */
-        const stockItem = document.createElement("div");
-        stockItem.classList.add("stock-item", "text-center", "flex", "flex-col", "items-center", "box-border", "h-44", "shadow-md", "bg-white", "w-44");
-
-        stockItem.innerHTML = `
+    stockItem.innerHTML = `
             <div>
                 <img class="h-20 w-28 object-scale-down object-center" src="${stock.image}" alt="amazon store">
                 <p class="text-md">${stock.price}</p>
@@ -54,6 +65,6 @@ document.addEventListener("DOMContentLoaded", function() {
             </div>
         `;
 
-        stockList.appendChild(stockItem);
-    });
+    stockList.appendChild(stockItem);
+  });
 });
